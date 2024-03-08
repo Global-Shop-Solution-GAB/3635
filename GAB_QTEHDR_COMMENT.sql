3a0a81061bddd455c1da5acd16822189f83d403d
Service Web <sw@gssmail.com>
2024-03-08 14:06:50 -0600

    Commit changes in C:\Websites\Git\emarlow\3635\GAB_QTEHDR_COMMENT.sql

---
+++ GAB_QTEHDR_COMMENT.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GAB_QTEHDR_COMMENT
+ * ========================================== */
+CREATE TABLE "GAB_QTEHDR_COMMENT"(
+ "QUOTE" LONGVARCHAR,
+ "GS_USER" LONGVARCHAR,
+ "TEXT1" LONGVARCHAR);
+
+
